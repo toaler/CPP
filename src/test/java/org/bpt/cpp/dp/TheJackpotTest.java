0a64bb7c886625427182c8a7b95abe413095b37a
toaler <brian.toal@gmail.com>
2014-06-11 08:34:16 -0700

    Solution to 'The Jackpot'.

---
+++ src/test/java/org/bpt/cpp/dp/TheJackpotTest.java
@@ -0,0 +1,40 @@
+package org.bpt.cpp.dp;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+import junit.framework.TestCase;
+
+public class TheJackpotTest extends TestCase {
+	
+	public void testInput() throws IOException {
+		String in = 
+				"5\n" + 
+				"12 -4\n" + 
+				"-10 4\n" + 
+				"9\n" + 
+				"3\n" + 
+				"-2 -1 -2\n" +
+				"15\n" +
+				"-3 4 8 -4 8 -5 3 4 8 -10 -2 -1 3 8 -6\n" +
+				"0\n";
+		
+		InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(in.getBytes()));
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+
+		TheJackpot.mainWork(new BufferedReader(isr), ps);
+		
+		String out =
+				"The maximum winning streak is 13.\n" +
+		        "Losing streak.\n" +
+		        "The maximum winning streak is 26.\n";
+		
+		assertEquals(out, baos.toString()); 
+	}
+}
